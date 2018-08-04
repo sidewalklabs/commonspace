cdabf256710d8913b6f5c5d4c23dbd8b412cb832
Eric Sebastian <sebastian@sidewalklabs.com>
2018-08-04 17:40:41 -0400

    adding study table

--- deployment/init.sql
+++ deployment/init.sql
@@ -7,3 +7,21 @@ CREATE TABLE IF NOT EXISTS users
 
 CREATE UNIQUE INDEX users_lower_email_unique_idx ON users (lower(email));
 
+CREATE SCHEMA IF NOT EXISTS data_collection;
+-- ALTER ROLE <your_login_role> SET search_path TO data_collection,"$user",public;
+
+CREATE TYPE studyScale AS ENUM ('district', 'city', 'cityCentre', 'neighborhood', 'blockScale', 'singleSite');
+
+CREATE TABLE IF NOT EXISTS data_collection.study
+(
+    study_id UUID PRIMARY KEY,
+    title TEXT,
+    project TEXT,
+    project_phase TEXT,
+    stateDate TIMESTAMP,
+    endDate TIMESTAMP,
+    scale studyScale,
+    userId UUID REFERENCES users(user_id) NOT NULL,
+    protocolVersion TEXT NOT NULL,
+    notes TEXT
+);
