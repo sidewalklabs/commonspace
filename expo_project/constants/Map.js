cf29cc68cf56a08ae42eb5e1161d32d6551b2a1a
Ananta Pandey <pandananta@users.noreply.github.com>
2018-09-13 19:33:25 -0400

    add dummy park features (#49)

--- expo_project/constants/Map.js
+++ expo_project/constants/Map.js
@@ -14,4 +14,30 @@ export default {
     { latitude: 43.703889, longitude: -79.343398 },
     { latitude: 43.703354, longitude: -79.343594 },
   ],
+  features: [
+    {
+      latitude: 43.703899,
+      longitude: -79.343813,
+      title: 'Trash can 1',
+      description: 'Placeholder. This is a demo of park features.',
+    },
+    {
+      latitude: 43.703772,
+      longitude: -79.343875,
+      title: 'Trash can 2',
+      description: 'Placeholder. This is a demo of park features.',
+    },
+    {
+      latitude: 43.703448,
+      longitude: -79.343689,
+      title: 'Park Café',
+      description: 'This is the shipping-container based cafe.',
+    },
+    {
+      latitude: 43.703539,
+      longitude: -79.343655,
+      title: 'Tandoor Oven',
+      description: 'This is fire oven used for baking bread.',
+    },
+  ],
 };
