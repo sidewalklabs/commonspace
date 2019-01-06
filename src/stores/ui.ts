9adb1a69a2c74c896244423db18a5944dbad0899
Eric Sebastian <sebastian@sidewalklabs.com>
2019-01-06 18:27:43 -0500

    admin ui snackbar

--- src/stores/ui.ts
+++ src/stores/ui.ts
@@ -10,18 +10,37 @@ export function visualizeNewStudy() {
     uiState.visibleModal = 'study';
 }
 
+
 export type AvailableModals = 'studyFields' | 'study' | 'surveyors' | 'surveys' | 'error' | null
+type AvailableSnackbarType = 'success' | 'error' | null
 
 interface UiState {
     availableLocations: AvailableLocation[]; 
     currentStudyIsNew: boolean;
     visibleModal: AvailableModals;
+    snackBar: {
+        snackbarType: AvailableSnackbarType;
+        snackbarText: string;
+    };
 }
 
+export function setSnackBar(snackbarType: AvailableSnackbarType, snackbarText: string) {
+    uiState.snackBar = {
+        snackbarType,
+        snackbarText
+    }
+}
+
+
+// TODO cascade setting of modal on setting errorText?
 const uiState: UiState = observable({
     availableLocations: [],
     currentStudyIsNew: false,
-    visibleModal: null
+    visibleModal: null,
+    snackBar: {
+        snackbarType: null,
+        snackbarText: '',
+    }
 });
 
 autorun(() => {
