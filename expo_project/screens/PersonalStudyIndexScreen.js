aacaa4577cda1b464c4c24ad26618b2a231bb811
Patrick Keenan <me@patrickkeenan.me>
2019-01-21 19:43:52 -0500

    Integrated styles to banner component
    Fixes for previous push to tweakl the zero state typography

--- expo_project/screens/PersonalStudyIndexScreen.js
+++ expo_project/screens/PersonalStudyIndexScreen.js
@@ -60,28 +60,13 @@ class PersonalStudyIndexScreen extends React.Component {
         {loading && <ActivityIndicator />}
         {!loading &&
           !studies.length && (
-            // <Banner
-            //   title="You do not have any studies"
-            //   description="You do not have any studies assigned to you currently. If you believe this is
-            //     incorrect, please reach out to your study coordinator. In the mean time, try a demo study"
-            //   cta="Try a demo"
-            //   style={styles.banner}
-            //   ctaOnPress={() => this.props.navigation.navigate('DemoStack')}
-            // />
-            <View style={styles.zeroMessage}>
-              <Title style={styles.title}>No Studies Found</Title>
-              <Subheading style={styles.description}>There are no studies assigned to you. If this is an error, reach out to your study coordinator.</Subheading>
-              <View style={styles.footer}>
-                <Button
-                  style={styles.button}
-                  raised
-                  color="#ffcf2b"
-                  theme={{ ...Theme, roundness: 12 }}
-                  onPress={() => this.props.navigation.navigate('DemoStack')}>
-                  <Text style={styles.buttonText}>Try the Demo</Text>
-                </Button>
-              </View>
-            </View>
+            <Banner
+              title="No Studies Found"
+              description="There are no studies assigned to you. If this is an error, reach out to your study coordinator."
+              cta="Try the Demo"
+              style={styles.banner}
+              ctaOnPress={() => this.props.navigation.navigate('DemoStack')}
+            />
         )}
         {!loading &&
           studies.length && <StudyFeed token={token} studies={studies} title="Your studies" />}
@@ -94,50 +79,6 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#FAFAFA',
     flex: 1,
-  },
-  banner: {
-    color: '#333333',
-    textAlign: 'center',
-    fontFamily: 'product-bold',
-    marginVertical: 20,
-  },
-  zeroMessage:{
-    marginLeft:24,
-    marginRight:24,
-    flex: 1,
-    justifyContent: 'center',
-    flex: 1,
-  },
-  title: {
-    color: '#333333',
-    textAlign: 'center',
-    fontFamily: 'product-bold',
-    fontSize: 36,
-    lineHeight:42,
-    marginBottom: 10,
-  },
-  description: {
-    color: '#333333',
-    textAlign: 'center',
-    fontSize: 16,
-    lineHeight:20,
-  },
-  button: {
-    marginTop: 20,
-    alignItems: 'center',
-    flexDirection: 'column',
-  },
-  buttonText: {
-    fontFamily: 'product-medium',
-    fontSize: 16,
-    height:48,
-    lineHeight:30,
-    letterSpacing: 0.5,
-  },
-  footer: {
-    padding: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
 });
 
