2b7c8ed53fb4e9bd96099f4c846d4955a80831d0
Eric Sebastian <sebastian@sidewalklabs.com>
2019-02-25 15:11:40 -0500

    cleaning login and signup mobx stores

--- src/stores/signup.ts
+++ src/stores/signup.ts
@@ -4,6 +4,7 @@ import snakecaseKeys from 'snakecase-keys';
 import authState from './auth';
 import { navigate } from './router';
 import uiState, { setSnackBar } from './ui';
+import { postRest } from '../client';
 
 const MAX_PASSWORD_LENGTH = 1000;
 const MIN_PASSWORD_LENGTH = 7;
@@ -72,7 +73,7 @@ export async function logInUserGoogleOAuth(response) {
 }
 
 export async function signUpUser() {
-    const { name, password, passwordConfirmation, email } = signUpState;
+    const { password, passwordConfirmation, email } = signUpState;
     if (password !== passwordConfirmation) {
         signUpState.passwordConfirmationErrorMessage = 'Passwords must match';
         setSnackBar('error', 'Passwords must match');
@@ -87,36 +88,24 @@ export async function signUpUser() {
         }
         return;
     }
-    const data = {
-        password,
-        email,
-        name
-    };
-    const { status, statusText } = await fetch(`/auth/signup`, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, cors, *same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, same-origin, *omit
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8'
-            // "Content-Type": "application/x-www-form-urlencoded",
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrer: 'no-referrer', // no-referrer, *client
-        body: JSON.stringify(snakecaseKeys(data)) // body data type must match "Content-Type" header
-    });
-    if (status === 200) {
+    try {
+        await postRest(`/auth/signup`, { password, email });
         authState.isAuth = true;
         navigate('/studies');
-    } else {
-        console.error(statusText);
-        setSnackBar('error', `Unable to Sign In ${statusText}`);
+    } catch (error) {
+        setSnackBar('error', `Unable to sign sign ${error}`);
+    } finally {
+        resetSignupState();
     }
+}
+
+function resetSignupState() {
+    signUpState.email = '';
+    signUpState.password = '';
 }
 
 interface SignUpState {
     email: string;
-    name: string;
     password: string;
     passwordConfirmation: string;
     emailErrorMessage: string;
@@ -126,7 +115,6 @@ interface SignUpState {
 
 const signUpState = observable({
     email: '',
-    name: '',
     password: '',
     passwordConfirmation: '',
     emailErrorMessage: '',
